d38f6c3ff96e1176bd145cf07f993488ed7450fe
bencullimore <ben.cullimore@nhs.net>
2018-04-12 09:14:08 +0100

    A-Z v1

---
+++ Acrivastine (Benadryl)_ antihistamine that relieves allergy symptoms - NHS.UK_files/wtid.js
@@ -0,0 +1,3 @@
+if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
+Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"f9e960fb-c04e-4f28-8f5a-aa38e86dd181","gTempWtId":"","gWtAccountRollup":"1"});
+}
